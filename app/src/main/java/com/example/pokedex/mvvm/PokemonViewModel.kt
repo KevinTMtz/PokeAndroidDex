eeb24d4805a0011f8c07c20d644063cf6de11816
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2022-06-03 17:21:17 -0500

    Load more pokemons on scroll

--- app/src/main/java/com/example/pokedex/mvvm/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokedex/mvvm/PokemonViewModel.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.pokedex.data.PokemonData
 import com.example.pokedex.data.PokemonInfo
 import com.example.pokedex.service.PokeMonApi
 import kotlinx.coroutines.launch
@@ -14,16 +13,19 @@ class PokemonViewModel: ViewModel() {
     var listaPokemonsInfo: List<PokemonInfo> by mutableStateOf(listOf())
     var errorMessage: String by mutableStateOf("")
 
-    fun getPokemons(){
+    fun getPokemons() {
         viewModelScope.launch {
             val apiService = PokeMonApi.getInstance()
+
             try{
-                val pokemons = apiService.getPokemonsList()
-                val pokemonsInfo = mutableListOf<PokemonInfo>()
+                val pokemons = apiService.getPokemonsList(listaPokemonsInfo.size + 20)
+                val pokemonsInfo: MutableList<PokemonInfo> = listaPokemonsInfo.toMutableList()
+
                 for(pokemon in pokemons.results){
                     val info = apiService.getPokemonInfo(pokemon.name)
                     pokemonsInfo.add(info)
                 }
+
                 listaPokemonsInfo = pokemonsInfo
             }
             catch (e: Exception){
--- app/src/main/java/com/example/pokedex/screens/PokemonsScreen.kt
+++ app/src/main/java/com/example/pokedex/screens/PokemonsScreen.kt
@@ -1,20 +1,23 @@
 package com.example.pokedex.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
@@ -23,7 +26,10 @@ import com.example.pokedex.mvvm.PokemonViewModel
 
 @Composable
 fun PokemonsScreen(navController: NavHostController) {
-    val pokemonViewModel: PokemonViewModel = PokemonViewModel()
+    val context = LocalContext.current
+    val pokemonViewModel = PokemonViewModel()
+    val scrollState = rememberLazyListState()
+
     Scaffold(topBar = {
         TopAppBar {
             Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back",
@@ -34,9 +40,33 @@ fun PokemonsScreen(navController: NavHostController) {
             Text(text = "Pokedex List")
         }
     }) {
-        Pokemons(pokemons = pokemonViewModel.listaPokemonsInfo)
-        pokemonViewModel.getPokemons()
+        Pokemons(scrollState = scrollState, pokemons = pokemonViewModel.listaPokemonsInfo)
 
+        if (pokemonViewModel.listaPokemonsInfo.isEmpty()) {
+            pokemonViewModel.getPokemons()
+        }
+
+        if (scrollState.isScrollInProgress) {
+            DisposableEffect(Unit) {
+                onDispose {
+                    if (scrollState.layoutInfo.visibleItemsInfo.lastOrNull()?.index == scrollState.layoutInfo.totalItemsCount - 1) {
+                        Toast.makeText(context, "Loaded more pokemons...", Toast.LENGTH_SHORT).show()
+                        pokemonViewModel.getPokemons()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Pokemons(scrollState: LazyListState, pokemons: List<PokemonInfo>) {
+    LazyColumn (
+        state = scrollState
+    ) {
+        itemsIndexed(items = pokemons) { _, item ->
+            Pokemon(pokemon = item)
+        }
     }
 }
 
@@ -77,16 +107,6 @@ fun Pokemon(pokemon: PokemonInfo) {
                         modifier = Modifier.fillMaxWidth()
                     )
                 }
-        }
-    }
-}
-
-@Composable
-private fun Pokemons(pokemons: List<PokemonInfo>) {
-    val scrollState = rememberScrollState()
-    LazyColumn {
-        itemsIndexed(items = pokemons) { index, item ->
-            Pokemon(pokemon = item)
         }
     }
 }
