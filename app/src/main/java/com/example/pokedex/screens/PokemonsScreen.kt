30d6bf768cd7996c894d4f1f97ac6e8060ab1e64
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2022-06-03 17:51:32 -0500

    Search bar

--- app/src/main/java/com/example/pokedex/screens/PokemonsScreen.kt
+++ app/src/main/java/com/example/pokedex/screens/PokemonsScreen.kt
@@ -1,28 +1,35 @@
 package com.example.pokedex.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.example.pokedex.data.PokemonInfo
 import com.example.pokedex.mvvm.PokemonViewModel
+import java.util.*
 
 @Composable
 fun PokemonsScreen(navController: NavHostController) {
@@ -40,13 +47,21 @@ fun PokemonsScreen(navController: NavHostController) {
             Text(text = "Pokedex List")
         }
     }) {
-        Pokemons(scrollState = scrollState, pokemons = pokemonViewModel.listaPokemonsInfo)
+        val (searchStr, onSearchStrChange) = remember { mutableStateOf("") }
+
+        Column {
+            SearchBar(value = searchStr, onValueChange = onSearchStrChange)
+
+            Pokemons(scrollState = scrollState, pokemons = pokemonViewModel.listaPokemonsInfo.filter { pokemonInfo ->  
+                pokemonInfo.name.startsWith(searchStr.lowercase(Locale.getDefault()))
+            })
+        }
 
         if (pokemonViewModel.listaPokemonsInfo.isEmpty()) {
             pokemonViewModel.getPokemons()
         }
 
-        if (scrollState.isScrollInProgress) {
+        if (scrollState.isScrollInProgress && searchStr.isEmpty()) {
             DisposableEffect(Unit) {
                 onDispose {
                     if (scrollState.layoutInfo.visibleItemsInfo.lastOrNull()?.index == scrollState.layoutInfo.totalItemsCount - 1) {
@@ -57,6 +72,27 @@ fun PokemonsScreen(navController: NavHostController) {
             }
         }
     }
+}
+
+@Composable
+private fun SearchBar(value: String, onValueChange: (String) -> Unit) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        textStyle = TextStyle(fontSize = 17.sp, color = Color.Black),
+        trailingIcon = { Icon(Icons.Filled.Search, null, tint = Color.DarkGray) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .background(MaterialTheme.colors.primary, RoundedCornerShape(16.dp)),
+        placeholder = { Text(text = "Search", color = Color.DarkGray) },
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            backgroundColor = Color.Transparent,
+            cursorColor = Color.DarkGray
+        )
+    )
 }
 
 @Composable
